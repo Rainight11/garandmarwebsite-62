68ba603ff7d09564faaa89ffb7821ccc989c9562
Rainight11 <jeremyschweigert@icloud.com>
2025-07-28 17:34:14 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: "Reviving the Golden Era of Music - GAR & MAR Records",
   generator: "v0.dev",
 icons: {
-    icon: "/favicon.ico", // Or .png or .svg — whatever you end up using
+    icon: "/public/favicon.ico", // Or .png or .svg — whatever you end up using
   },
 };
 
